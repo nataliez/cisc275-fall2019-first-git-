c59a6357c4926dbfe5a2f7e5822f5810df72967f
Sydney C <saclasen@udel.edu>
2019-11-04 12:20:36 -0500

    some work on the Cave class as a tracker

--- Estuary Game/src/Cave.java
+++ Estuary Game/src/Cave.java
@@ -1,5 +1,17 @@
-
+/**
+ * A non-physical object that keeps track of the pieces of debris left in the cave pathway.
+ * @author CISC275 Team 6
+ *
+ */
 public class Cave {
-
+	
 	int trashLevel;
+	
+	public Cave(){
+		trashLevel = 10;
+	}
+	
+	public int getTrashLevel() {
+		return trashLevel;
+	}
 }
