8858d36e6400e1180dbb891e3a36a05ce652c092
Sydney C <saclasen@udel.edu>
2019-11-04 12:33:57 -0500

    Possibly (???) finished the cave class

--- Estuary Game/src/Cave.java
+++ Estuary Game/src/Cave.java
@@ -4,14 +4,44 @@
  *
  */
 public class Cave {
-	
+	/**
+	 * The "level" of trash present in the Cave environment, out of 10.
+	 */
 	int trashLevel;
 	
+	/**
+	 * Empty constructor, sets trashLevel to 10.
+	 */
 	public Cave(){
 		trashLevel = 10;
 	}
 	
+	/**
+	 * Returns the current trashLevel
+	 */
 	public int getTrashLevel() {
 		return trashLevel;
 	}
+	
+	/**
+	 * Reduces trashLevel by one upon the successful collision of corresponding garbage draggable object and collection object. (i.e. trash and 
+	 * trash bag.)
+	 */
+	public void reduceTrashLevel() {
+		trashLevel --;
+	} //TODO call this method upon "corresponding collisions" in the trash minigame.
+	
+	/**
+	 * Boolean that tests if the cave has been cleared out.
+	 * @return true if the trashLevel is 0
+	 * @return false if the trashLevel is anything but 0
+	 */
+	public boolean caveClear() {
+		if(this.trashLevel == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	
 }
