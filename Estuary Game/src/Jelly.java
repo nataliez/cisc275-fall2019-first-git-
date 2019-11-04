c9f5c2523eb920f00cdcb331febda5dc82a41100
Sydney C <saclasen@udel.edu>
2019-11-04 11:55:33 -0500

    some updates on jelly

--- Estuary Game/src/Jelly.java
+++ Estuary Game/src/Jelly.java
@@ -1,5 +1,16 @@
 
 public class Jelly extends MoveObject{
 
-	int score;
+	int score = 1;
+    int imgHeight = 50;
+    int imgWidth = 50;
+	
+	public Jelly(double yPos, double xPos, Direction currDirection,int velX, int velY) {
+		this.velX = velX; this.velY = velY; this.currDirection = currDirection;  this.xPos = xPos;
+		this.yPos = yPos;
+	}
+	
+	public Jelly() {
+		
+	}
 }
