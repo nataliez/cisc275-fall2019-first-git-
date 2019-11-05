03a35ab6b0d2f6d6674defbf99a64a946c3718b6
Alyssa Burks <burksan@udel.edu>
2019-11-05 15:59:58 -0500

    Alyssa: checking to see if git is correct. ControlObject now has an author

--- Estuary Game/src/ControlObject.java
+++ Estuary Game/src/ControlObject.java
@@ -1,4 +1,8 @@
-
+/**
+ * A non-physical object that keeps track of the pieces of debris left in the cave pathway.
+ * @author CISC275 Team 6
+ *
+ */
 public class ControlObject extends MoveObject {
 	
 	Direction currDir;
